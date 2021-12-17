2940d1e3a797765b3b7bbde05801f41f6b553f60
mansouur <mansouralsharifit@gmail.com>
2021-12-17 09:18:58 -0600

    server1

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const jsonServer = require('json-server');
+const app = jsonServer.create();
 const path = require('path');
-const app = express();
+const express = require('express');
+const middlewares = jsonServer.defaults();
+const router = jsonServer.router('db.json');
+const port = process.env.PORT || 3001;
+
+app.use('data/db.json', middlewares, router);
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(process.env.PORT || 8080);
+server.listen(port);
